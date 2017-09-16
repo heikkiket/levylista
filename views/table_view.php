5d62e3db99bedba57e42cfc20a7948289e852680
Heikki Ketoharju <heikki.ketoharju@iki.fi>
2017-09-16 14:48:52 +0300

    fix aloitustagi

--- views/table_view.php
+++ views/table_view.php
@@ -15,13 +15,13 @@ if(!isset($query)) global $query;
 <th><?php _e('Julkaisuvuosi', 'levylista') ?></th>
 <th><?php _e('Lisätty listaan', 'levylista') ?></th>
 </tr>
-<?php 
+<?php
 while ( $query->have_posts() ) {
     $query->the_post();
     $levydata = get_post_custom();
     ?>
     <tr>
-        <td><a href="<?levylista_the_artist_url() ?>"><?php levylista_the_artist() ?></a></td>
+        <td><a href="<?php levylista_the_artist_url() ?>"><?php levylista_the_artist() ?></a></td>
         <td><a href="<?php the_permalink() ?>"><?php the_title()?></a></td>
         <td><a href="<?php levylista_the_year_url() ?>"><?php levylista_the_year() ?></a></td>
         <td><?php the_time('d.m.Y') ?></td>
